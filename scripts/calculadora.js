284eccb4eda944d700cffe5cea9e9f613d85933d
Arruda Costa <arrudafilho@icloud.com>
2024-02-10 17:48:06 -0300

    v

---
+++ scripts/calculadora.js
@@ -0,0 +1,89 @@
+// Muda o cenário conforme rolagem
+document.querySelector(`.centro_pagina`).addEventListener('scroll', () => {
+    let lista = document.querySelector(`.centro_pagina`)
+    let sec_calculadora = document.querySelector(`#sec_calculadora`)
+    let alt_sec_na_lista = Math.abs(sec_calculadora.parentElement.offsetTop - sec_calculadora.offsetTop)
+    let alt_secao = document.querySelector(`#sec_calculadora`).offsetHeight
+
+    // Saiu da seção
+    if(lista.scrollTop >= alt_sec_na_lista + alt_secao) {
+        return false
+    }
+    // Entrou na seção
+    else if(lista.scrollTop >= (alt_sec_na_lista - alt_secao)) {
+        
+        // Prepara a nova rotação
+        let graus_por_pixel = 180 / (alt_secao * 2)
+        let angulo = ((lista.scrollTop - alt_sec_na_lista + alt_secao) * graus_por_pixel) - 90
+        
+        // Prepara a nova escala
+        let escala_por_pixel = 1 / (alt_secao * 2)
+        let escala = ((lista.scrollTop - alt_sec_na_lista + alt_secao) * escala_por_pixel) + 0.5
+        
+        // Aplica na calculadora
+        document.querySelector(`.conj_calculadora`).style.transform = `rotate(${angulo}deg) scale(${escala})`
+
+    }
+
+})
+
+
+
+// Exibir aba de histórico
+let exib_mensag_historic = true
+document.addEventListener('click', (e) => {
+    let item = (e.target).closest(`#bt_aba_historico`)
+
+    if(item !== null) {
+        let aba_histor = document.querySelector(`.aba_historico`)
+
+        // Abre a aba
+        if(!aba_histor.classList.contains('ativo')) {
+            aba_histor.classList.add('ativo')
+
+            // Exibe a mensagem
+            if(exib_mensag_historic) {
+
+                // Desativa a re-apresentação da mensagem
+                exib_mensag_historic = false
+    
+                // Apresenta a mensagem
+                setTimeout(() => {
+                    aba_histor.querySelector(`.mensagem_anim`).classList.add('ativo')
+                    setTimeout(() => {
+                        aba_histor.querySelector(`.mensagem_anim`).classList.remove('ativo')
+        
+                        
+                    }, 3000)
+                }, 1500)
+    
+            }
+
+        }
+
+    }
+    else {
+        let aba_histor = document.querySelector(`.aba_historico`)
+
+        // Fecha a aba e remove a mensagem
+        if(aba_histor !== null) {
+            aba_histor.classList.remove('ativo')
+            aba_histor.querySelector(`.mensagem_anim`).classList.remove('ativo')
+        }
+
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,56 @@
-console.log('script funcionando')
+// Muda a barra de progresso lateral conforme o scroll
+document.querySelector(`.centro_pagina`).addEventListener('scroll', () => {
+    let secao_aberta = Array.from(document.querySelectorAll(`.secao_pagina`)).filter(x => x.getBoundingClientRect().top - 30 <= document.querySelector(`.area_util`).getBoundingClientRect().top && x.getBoundingClientRect().top + 30 >= document.querySelector(`.area_util`).getBoundingClientRect().top)[0]
+    let todas_secoes = Array.from(document.querySelectorAll(`.secao_pagina`))
+    let index_secao = todas_secoes.indexOf(secao_aberta)
+
+    // Verifica se o index esta correto
+    if(index_secao !== -1) {
+
+        // Verifica se é a primeira pagina
+        if(index_secao === 0) {
+            
+            // Oculta a barra de progresso
+            document.querySelector(`.aba_paginacao_vertic`).classList.remove('ativo')
+
+        }
+        else if(index_secao === 1) {
+
+            // Oculta a barra de progresso
+            document.querySelector(`.aba_paginacao_vertic`).classList.remove('ativo')
+            
+        }
+        else if(index_secao === 2) {
+
+            // Exibe a barra de progresso
+            document.querySelector(`.aba_paginacao_vertic`).classList.add('ativo')
+
+            // Remove destaque dos pinos
+            document.querySelectorAll(`.pino_paginacao`).forEach(x => x.classList.remove('ativo'))
+    
+            // Ativa o pino correspondente a página
+            document.querySelectorAll(`.pino_paginacao`)[0].classList.add('ativo')
+            
+        }
+        else if(index_secao === 3) {
+
+            // Exibe a barra de progresso
+            document.querySelector(`.aba_paginacao_vertic`).classList.add('ativo')
+
+            // Remove destaque dos pinos
+            document.querySelectorAll(`.pino_paginacao`).forEach(x => x.classList.remove('ativo'))
+    
+            // Ativa o pino correspondente a página
+            document.querySelectorAll(`.pino_paginacao`)[1].classList.add('ativo')
+            
+        }
+        else {
+
+            // Oculta a barra de progresso
+            document.querySelector(`.aba_paginacao_vertic`).classList.remove('ativo')
+
+        }
+
+    }
+
+})
